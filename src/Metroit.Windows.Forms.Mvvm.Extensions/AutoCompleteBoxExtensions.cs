285764cc0112f6dbccddcbe5adf3cbd797e41189
takiru0105 <cherrysunburst@gmail.com>
2025-07-19 00:50:11 +0900

    Metroit.Windows.Forms.Mvvm.Extensions を追加。未テスト。

---
+++ src/Metroit.Windows.Forms.Mvvm.Extensions/AutoCompleteBoxExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Metroit.Windows.Forms.Mvvm.Extensions
+{
+    /// <summary>
+    /// オートコンプリートボックスのバインドを行う拡張メソッドを提供します。
+    /// </summary>
+    public static class AutoCompleteBoxExtensions
+    {
+        /// <summary>
+        /// データソースをバインドします。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="autoCompleteBox">オートコンプリートボックスオブジェクト。</param>
+        /// <param name="expression">バインドする値の式木。</param>
+        public static void BindDataSource<T>(this AutoCompleteBox autoCompleteBox, Expression<Func<T>> expression)
+        {
+            PropertyBindExtensions.Bind(() => autoCompleteBox.DataSource, expression);
+        }
+    }
+}
